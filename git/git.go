e1327848a594ae44c06eb2a907bfe357c6324f2b
Matteo Gassend <mgassend@gmail.com>
2024-07-13 12:46:44 +0200

    add: trying with no auth

--- git/git.go
+++ git/git.go
@@ -5,26 +5,25 @@ import (
 	"github.com/go-git/go-git/v5/plumbing"
 
 	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/matfire/hammer/types"
 )
 
 func Pull(config types.Config, app types.App, releasePayload types.GithubReleasePayload) {
-	auth := &http.BasicAuth{
+	/*auth := &http.BasicAuth{
 		Username: config.Username,
 		Password: config.Password,
-	}
+	}*/
 	repo, err := git.PlainOpen(app.Path)
 	if err != nil {
 		panic("path is not a valid repo")
 	}
-	fmt.Println(repo)
 	workTree, err := repo.Worktree()
 	if err != nil {
 		panic("could not get worktree")
 	}
-	err = workTree.Pull(&git.PullOptions{Auth: auth})
+	err = workTree.Pull(&git.PullOptions{})
 	if err != nil {
+		fmt.Println(err)
 		panic("could not pull")
 	}
 	err = workTree.Checkout(&git.CheckoutOptions{Branch: plumbing.ReferenceName(releasePayload.Release.TagName)})
